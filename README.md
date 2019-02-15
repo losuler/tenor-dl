2431a4f91eeac9c6702c1985ff7c7cc8f471704f
losuler <1330924-losuler@users.noreply.gitlab.com>
2019-02-15 14:33:03 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# About
+
+Allows a way to download .gif files from [Tenor](https://tenor.com/), which is presumably intentionally difficult or impossible to do via a web browser.
+
+# Dependancies
+
+```
+requests
+clint
+```
+
+# Configuration
+
+A developer API key is **required** ([see here](https://tenor.com/gifapi/documentation#quickstart-setup)). This is to be provided in `config.ini` (see `config.ini.example`) or as an optional argument.
+
+# Usage
+
+```
+tenor-dl.py [-h] [-a APIKEY] [-d] id
+```
+
+## Positional arguments
+
+```
+id	the ID of the gif
+```
+
+The id for the gif can be found at the end of the URL on the landing page or view page for the gif. For example the id for this gif is `6198981`:
+
+`https://tenor.com/view/kitty-highkitten-mdmacat-cat-happykitty-gif-6198981`
+
+`https://media1.tenor.com/images/f6fe8d1d0463f4e51b6367bbecf56a3e/tenor.gif?itemid=6198981`
+
+## Optional arguments
+
+```
+-h, --help                 	show this help message and exit
+-a APIKEY, --apikey APIKEY 	the Tenor api key
+-d, --download             	download the gif
+```
---
+++ tenor-dl.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import requests
+from clint.textui import progress
+
+from urllib.parse import urlparse
+import argparse
+import configparser
+import sys
+import os
+import re
+
+ERROR = "\033[91m" + "[ERROR]" + "\033[0m"
+
+def argparse_init():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("id", help="the ID of the gif")
+    parser.add_argument("-a", "--apikey", help="the Tenor api key")
+    parser.add_argument("-d", "--download", help="download the gif", action="store_true")
+    args = parser.parse_args()
+    return args
+
+def parse_config():
+    config = configparser.ConfigParser()
+    try:
+        config.read('config.ini')
+        api_key = config['TENOR']['API_KEY']
+    except KeyError:
+        sys.exit("{ERROR} Please check config.ini".format(ERROR=ERROR))
+    return api_key
+
+def request_gif(gif_id, api_key):
+    resp = requests.get("https://api.tenor.com/v1/gifs?ids={id}&key={key}"
+                        .format(id=gif_id, key=api_key)).json()
+    gif_url = resp['results'][0]['media'][0]['gif']['url']
+
+    title = resp['results'][0]['title']
+    url = resp['results'][0]['itemurl']
+
+    if not title == "":
+        gif_name = title
+    else:
+        # Get the filename from the URL path name.
+        url_path = urlparse(url).path
+        gif_name = re.search(r"/view/(.*)-gif", url_path).group(1)
+
+    return gif_url, gif_name
+
+def download_gif(gif_url, gif_name):
+    req = requests.get(gif_url, stream=True)
+    file_name = "{gif_name}.gif".format(gif_name=gif_name)
+
+    with open(file_name, 'wb') as f:
+        total_length = int(req.headers.get('content-length'))
+        for chunk in progress.bar(req.iter_content(chunk_size=1024),
+                                  expected_size=(total_length/1024) + 1):
+            if chunk:
+                f.write(chunk)
+                f.flush()
+
+def main():
+    args = argparse_init()
+    gif_id = args.id
+
+    if args.apikey:
+        api_key = args.apikey
+    elif os.path.isfile("config.ini"):
+        api_key = parse_config()
+    else:
+        sys.exit("{ERROR} Must provide api key either as argument or in config.ini"
+                 .format(ERROR=ERROR))
+
+    gif_url, gif_name = request_gif(gif_id, api_key)
+
+    if args.download:
+        download_gif(gif_url, gif_name)
+
+    print("TITLE: {name}".format(name=gif_name))
+    print("URL: {url}".format(url=gif_url))
+
+if __name__ == "__main__":
+    main()
